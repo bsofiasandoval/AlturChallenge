6182a5e6a7b84fd4f1d03249629e00295df1a748
bsofiasandoval <bsofiasandoval2@gmail.com>
2025-11-23 15:10:14 -0500

    Debugging Railway

--- altur_challenge_be/main.py
+++ altur_challenge_be/main.py
@@ -110,6 +110,6 @@ def upload():
     except Exception as e:
         print(f"ERROR during upload: {str(e)}")
         return jsonify({'error': 'Internal server error','details': str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
